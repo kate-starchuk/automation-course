c88d77855a6e3a1b3209ffd00da093861387657a
Kate Starchuk <starchuk.kate@gmail.com>
2022-12-29 21:13:04 +0100

    Comlete first homework

---
+++ main.py
@@ -0,0 +1,23 @@
+'''
+Створіть дві змінні first=10, second=30. Виведіть на екран результат математичної
+взаємодії (+, -, *, / и тд.) для цих чисел.
+'''
+first = 10
+second = 30
+print(first + second)
+print(first - second)
+print(first * second)
+print(first / second)
+'''
+Створіть змінну и по черзі запишіть в неї результат порівняння (<, > , ==, !=) чисел з завдання 1.
+Виведіть на екран результат кожного порівняння.
+'''
+comparison = first > second
+print(comparison)
+comparison = first < second
+print(comparison)
+comparison = first == second
+print(comparison)
+comparison = first != second
+print(comparison)
+
