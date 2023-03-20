8ddab0531fbec438627f1e9a01b5d31b2b200088
Kate Starchuk <starchuk.kate@gmail.com>
2023-03-20 16:15:20 +0100

    Add to 15-th homework

---
+++ 15-th homework/tests.py
@@ -0,0 +1,20 @@
+from time import sleep
+
+from selenium.webdriver import Chrome, Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+
+
+def test_1():
+    driver = Chrome("drivers/chromedriver")
+    driver.get("https://www.yakaboo.ua/ua/knigi.html")
+    search_input_locator = '//input[@id="search"]'
+    first_list_item_locator = '//div[@class="ui-search-result__products"]//div[@class="ui-search-product"]/a'
+    search_input: WebElement = driver.find_element(By.XPATH, search_input_locator)
+    search_input.send_keys("Чорна рада, Пантелеймон Куліш")
+    sleep(1)
+    first_list_item: WebElement = driver.find_element(By.XPATH, first_list_item_locator)
+    first_list_item.click()
+    driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.PAGE_DOWN)
+    sleep(3)
+    driver.quit()
